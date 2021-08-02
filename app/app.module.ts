210693faa96c571aca79a16b93ca1da02f43e8a2
M-UNSA <mcentenoqu@unsa.edu.pe>
2021-08-02 09:40:41 -0500

    Implementando componente/app hola-mundo con comando ng generate component about

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,12 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { HelloWorld } from './primercomponente/primer.component';
+import { HolaMundoComponent } from './hola-mundo/hola-mundo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloWorld
+    HolaMundoComponent
   ],
   imports: [
     BrowserModule,
