63c95f710587cdf9ed7e94f295598cb2afbffbb3
M-UNSA <mcentenoqu@unsa.edu.pe>
2021-08-02 09:16:22 -0500

    Añadiendo múltiples componenentes y probando la tilde de ts para código en líneas múltiples

--- app/primercomponente/primer.component.ts
+++ app/primercomponente/primer.component.ts
@@ -2,8 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'hello-world',
-    template: '<h1>Hola Mundo</h1>',
-    styles: ['h1 {background:#000; color:#fff; }']
+    template: `
+    <div class="hello-world">
+        <h1>Hola Mundo</h1>
+        <h2>Subtítulo</h2>
+    </div>
+    `,
+    styles: ['.hello-world {background:#000; color:#fff; }']
 })
 export class HelloWorld {
 
