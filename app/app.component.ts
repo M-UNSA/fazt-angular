1e5d0e5045d71b83f47bd075d35f6597918e4842
M-UNSA <mcentenoqu@unsa.edu.pe>
2021-08-02 09:51:55 -0500

    Probando *ngIf

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  users = ['ryan', 'joe', 'cameron'];
+  users = ['ryan', 'joe', 'cameron', 'john', 'bruce'];
+  activated = false;
 }
